2ab63002b6645ecaad7e4b07f6f84d9ff2643eb1
cococrunchz <76513885+cococrunchz@users.noreply.github.com>
2021-01-19 12:27:43 +0800

    added buttons

--- app/src/main/java/com/example/pelino_calculator/MainActivity.java
+++ app/src/main/java/com/example/pelino_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pelino_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnSub,btnMulti,btnDiv,btnCe,btnAns,btnPeriod,btnChange,btnTxtdis;
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMulti = findViewById(R.id.btnMulti);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnCe = findViewById(R.id.btnCe);
+        btnAns = findViewById(R.id.btnAns);
+        btnPeriod = findViewById(R.id.btnPeriod);
+        btnChange = findViewById(R.id.btnChange);
+
+        btn0.setOnClickListener();
     }
+
 }
